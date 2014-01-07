7c56d9493467163122b63d47bd6d534c6622ac63
Fabio Pedrazzoli <fabio.pedrazzoli@vivomiles.com>
2014-01-07 11:30:43 +0000

    s3-log-rotator

---
+++ s3-log-rotator.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e                                                                                                       
+
+DATE=`date +%Y-%m-%d`
+S3CMD=/usr/bin/s3cmd
+
+# S3 bucket name without the s3 url
+S3BUCKETNAME="my.s3.bucket.name"
+LOGDIR=/var/log/s3/${S3BUCKETNAME}
+
+# check if s3cmd is installed and configured
+${S3CMD} ls > /dev/null || (echo 's3cmd needs to be installed and configured'; exit 1)
+
+# create todays' logdir                                                                                              
+mkdir -p ${LOGDIR}/${DATE}
+
+# download todays' files                                                                                             
+if ${S3CMD} sync --recursive s3://${S3BUCKETNAME}/ ${LOGDIR}/${DATE}
+then
+    # cleanup todays' files in S3                                                                                    
+    ${S3CMD} del s3://${S3BUCKETNAME}/*
+fi
+
+# concatenate today's files in one logfile                                                                           
+> ${LOGDIR}/${DATE}.log
+# a placeholder file is needed to avoid the error of catting nothing if there are no logs                            
+> ${LOGDIR}/${DATE}/placeholder
+
+if cat ${LOGDIR}/${DATE}/* >> ${LOGDIR}/${DATE}.log
+then
+    # remove all the local small logfiles and dir                                                                    
+    rm /var/log/s3/${S3BUCKETNAME}/${DATE}/ -rf
+fi
+
+# rotate old logs                                                                                                    
+[ -f ${LOGDIR}/today.log ] && mv ${LOGDIR}/today.log ${LOGDIR}/yesterday.log
+
+# put the log where awstats is supposed to find it                                                                   
+cp -av ${LOGDIR}/${DATE}.log ${LOGDIR}/today.log
+
+# compress old logs                                                                                                  
+gzip -9f ${LOGDIR}/${DATE}.log
